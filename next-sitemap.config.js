8e1769394aa11463ee59877bb60582472dca6224
Acunnia <44024205+Acunnia@users.noreply.github.com>
2024-07-11 19:38:34 +0200

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://devfest24.gdgsevilla.com/',
+  generateRobotsTxt: true, 
+  exclude: [''],
+  sitemapSize: 5000, // Tamaño máximo del sitemap
+}
